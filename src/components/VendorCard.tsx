3e254afefed471f8bd439832cdc0da1a3fe0642e
Justin Kugiyama <jkugi@hawaii.edu>
2024-11-19 18:19:25 -1000

    fixing the linking to vendor/store page

--- src/components/VendorCard.tsx
+++ src/components/VendorCard.tsx
@@ -5,17 +5,17 @@ import { Card, Image } from 'react-bootstrap';
 import Link from 'next/link';
 
 const VendorCard = ({ vendor }: { vendor: Vendor }) => (
-  <Link href={vendor.website} passHref>
-    <Card className="h-200">
-      <Card.Header className="text-center">
-        <Card.Title>
-          <strong>{vendor.name}</strong>
-        </Card.Title>
+  <Card className="h-200">
+    <Card.Header className="text-center">
+      <Card.Title>
+        <strong>{vendor.name}</strong>
+      </Card.Title>
+      <Link href={vendor.website} passHref>
         <Image src={vendor.image} height={150} className="mx-auto mb-3" />
-        <Card.Subtitle>{vendor.address}</Card.Subtitle>
-      </Card.Header>
-    </Card>
-  </Link>
+      </Link>
+      <Card.Subtitle>{vendor.address}</Card.Subtitle>
+    </Card.Header>
+  </Card>
 );
 
 export default VendorCard;
